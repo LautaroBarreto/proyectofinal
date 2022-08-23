fa9ee87e2fa6a31b4930f808075c5d4b5d13bc8a
Lautaro Barreto <102369702+LautaroBarreto@users.noreply.github.com>
2022-08-23 20:37:47 -0300

    Add files via upload

--- Js/arraysbarreto.js
+++ Js/arraysbarreto.js
@@ -1,53 +1,80 @@
-//......
-/*
-let option = parseInt(prompt(`Ingrese una opcion:
-                        \n1. Mostrar Orden de Sostenidos.
-                        \n2. Mostrar Orden de Bemoles.
-                        \n3. Mostrar Escalas Mayores con Sostenidos.
-                        \n4. Mostrar Escalas Mayores con Bemoles.
-                        \n5. Mostrar Escalas Menores Naturales con Sostenidos.
-                        \n6. Mostrar Escalas Menores Naturales con Bemoles.
-                        \n7. Salir del Menu.`)
-)
+class Escalas {
 
-
-while(option != 7) {
-    switch (option) {
-        case 1:
-            alert("El orden de Sostenidos es: " + ordenSostenidos);
-            break;
-        case 2:
-            alert("El orden de Bemoles es: " + ordenBemoles);
-            break;
-        case 3:
-            alert("Las Escalas Mayores con Sostenidos son: " + escalasMayConSos);
-            break;
-        case 4:
-            alert("Las Escalas Mayores con Bemoles son: " + escalasMayConBem);
-            break;
-        case 5:
-            alert("Las Escalas Menores Naturales con Sostenidos son: " + escalasMenNatConSos);
-            break;
-        case 6:
-            alert("Las Escalas Menores Naturales con Bemoles son: " + escalasMenNatConBem);
-        default:
-            break;
+    constructor(notas, modo, alteracion){
+        this.notas = notas.toUpperCase();
+        this.modo = modo.toUpperCase(); 
+        this.alteracion = alteracion.toUpperCase();
     }
-    option = parseInt(prompt(`Ingrese una opcion:
-                        \n1. Mostrar Orden de orden Sostenidos.
-                        \n2. Mostrar Orden de Bemoles.
-                        \n3. Mostrar Escalas Mayores con Sostenidos.
-                        \n4. Mostrar Escalas Mayores con Bemoles.
-                        \n5. Mostrar Escalas Menores Naturales con Sostenidos.
-                        \n6. Mostrar Escalas Menores Naturales con Bemoles.
-                        \n7. Salir del Menu.`)     
-    )    
-} 
-*/
+}
+
+let ordenSostenidos = new Escalas("FA#, DO#, SOL#, RE#, LA#, MI#, SI#", 'No posee', 'sostenidos');
+let ordenBemoles = new Escalas("SIb, MIb, LAb, REb, SOLb, DOb, FAb", 'No posee', 'bemoles');
+
+//escalas mayores con sostenido
+//reemplazada con array asociativo
+
+let escalaDo = new Escalas("DO, RE, MI, FA, SOL, LA, SI", 'mayor', 'sostenidos');
+let escalaRe = new Escalas("RE, MI, FA#, SOL, LA, SI, DO#", 'mayor', 'sostenidos');
+let escalaSol = new Escalas("SOL, LA, SI, DO, RE, MI, FA#", 'mayor', 'sostenidos');
+let escalaLa = new Escalas("LA, SI, DO#, RE, MI, FA#, SOL#", 'mayor', 'sostenidos');
+let escalaMi = new Escalas("MI, FA#, SOL#, LA, SI, DO#, RE#", 'mayor', 'sostenidos');
+let escalaSi = new Escalas("SI, DO#, RE#, MI#, FA#, SOL#, LA#", 'mayor', 'sostenidos');
+let escalaFaSostenido = new Escalas("FA#, SOL#, LA#, SI, DO#, RE#, MI#", 'mayor', 'sostenidos');
+let escalaDoSostenido = new Escalas("DO#, RE#, MI#, FA#, SOL#, LA#, SI#", 'mayor', 'sostenidos');
 
 
 
+//escalas mayores con bemoles
+let escalaFa = new Escalas("FA, SOL, LA, SIb, DO, RE, MI", 'mayor', 'bemoles');
+let escalaSib = new Escalas("SIb, DO, RE, MIb, FA, SOL, LA", 'mayor', 'bemoles');
+let escalaMib = new Escalas("MIb, FA, SOL, LAb, SIb, DO, RE", 'mayor', 'bemoles');
+let escalaLab = new Escalas("LAb, SIb, DO, REb, MIb, FA, SOL", 'mayor', 'bemoles');
+let escalaReb = new Escalas("REb, MIb, FA, SOLb, LAb, SIb, DO", 'mayor', 'bemoles');
+let escalaSolb = new Escalas("SOLb, LAb, SIb, DOb, REb, MIb, FA", 'mayor', 'bemoles');
+let escalaDob = new Escalas("DOb, REb, MIb, FAb, SOLb, LAb, SIb", 'mayor', 'bemoles');
+
+//escalas menores naturales con sostenidos
+let escalaLaMenor = new Escalas("LA, SI, DO, RE, MI, FA, SOL", "menor", "sostenidos");
+let escalaMiMenor = new Escalas("MI, FA#, SOL, LA, SI, DO, RE", "menor", "sostenidos");
+let escalaSiMenor = new Escalas("SI, DO#, RE, MI, FA#, SOL, LA", "menor", "sostenidos");
+let escalaFaSosMenor = new Escalas("FA#, SOL#, LA, SI, DO#, RE, MI", "menor", "sostenidos");
+let escalaDoSosMenor = new Escalas("DO#, RE#, MI, FA#, SOL#, LA, SI", "menor", "sostenidos");
+let escalaSolSosMenor = new Escalas("SOL#, LA#, SI, DO#, RE#, MI, FA#", "menor", "sostenidos");
+let escalaReSosMenor = new Escalas("RE#, MI#, FA#, SOL#, LA#, SI, DO#", "menor", "sostenidos");
+let escalaLaSosMenor = new Escalas("LA#, SI#, DO#, RE#, MI#, FA#, SOL#", "menor", "sostenidos");
+
+//escalas menores naturales con bemoles
+
+let escalaReMenor = new Escalas("RE, MI, FA, SOL, LA, SIb, DO", "menor", "bemoles");
+let escalaSolMenor = new Escalas("SOL, LA, SIb, DO, RE, MIb, FA", "menor", "bemoles");
+let escalaDoMenor = new Escalas("DO, RE, MIb, FA, SOL, LAb, SIb", "menor", "bemoles");
+let escalaFaMenor = new Escalas("FA, SOLb, LAb, SIb, DO, REb, MIb", "menor", "bemoles");
+let escalaSibMenor = new Escalas("SIb, DO, REb, MIb, FA, SOLb, LAb", "menor", "bemoles");
+let escalaMibMenor = new Escalas("MIb, FA, SOLb, LAb, SIb, DOb, REb", "menor", "bemoles");
+let escalaLabMenor = new Escalas("LAb, SIb, DOb, REb, MIb, FAb, SOLb", "menor", "bemoles");
 
 
-//escalas mayores
+//Agrupo las variables creadas con array asociativo, y de esta manera ahorro codigo funcional y puedo utilizar las diferentes escalas por separado mucho mejor.
+//Proximamente pasare cada escala a un array diferente.
 
+let escalasMayConSos = [escalaDo, escalaSol, escalaRe, escalaLa, escalaMi, escalaSi, escalaFaSostenido, escalaDoSostenido];
+let escalasMayConBem = [escalaFa, escalaSib, escalaMib, escalaLab, escalaReb, escalaSolb, escalaDob];
+let escalasMenNatConSos = [escalaLaMenor, escalaMiMenor, escalaSiMenor, escalaFaSosMenor, escalaDoSosMenor, escalaSolSosMenor, escalaReSosMenor, escalaLaSosMenor];
+let escalasMenNatConBem = [escalaReMenor, escalaSolMenor, escalaFaMenor,escalaSibMenor, escalaMibMenor, escalaLabMenor];
+
+
+//....
+
+let sostenidos = [escalaDo, escalaSol, escalaRe, escalaLa, escalaMi, escalaSi, escalaFaSostenido, escalaDoSostenido, escalaLaMenor, escalaMiMenor, escalaSiMenor, escalaFaSosMenor, escalaDoSosMenor, escalaSolSosMenor, escalaReSosMenor, escalaLaSosMenor];
+
+
+let bemoles = [escalaFa, escalaSib, escalaMib, escalaLab, escalaReb, escalaSolb, escalaDob, escalaReMenor, escalaSolMenor, escalaDoMenor, escalaFaMenor, escalaSibMenor, escalaMibMenor, escalaLabMenor]
+
+let cromatico = [ordenBemoles, ordenSostenidos]
+//....
+
+
+const todasLasEscalas = [ordenSostenidos, ordenBemoles, escalaDo, escalaSol, escalaRe, escalaLa, escalaMi, escalaSi, escalaFaSostenido, escalaDoSostenido, escalaFa, escalaSib, escalaMib, escalaLab, escalaReb, escalaSolb, escalaDob, escalaLaMenor, escalaMiMenor, escalaSiMenor, escalaFaSosMenor, escalaDoSosMenor, escalaSolSosMenor, escalaReSosMenor, escalaLaSosMenor, escalaReMenor, escalaSolMenor, escalaDoMenor, escalaFaMenor, escalaSibMenor, escalaMibMenor, escalaLabMenor]; 
+//...................
+
+console.log(cromatico);
--- Js/domEventosBarretoLautaro.js
+++ Js/domEventosBarretoLautaro.js
@@ -1,104 +1,27 @@
-class Escalas {
-
-    constructor(notas, modo, alteracion){
-        this.notas = notas.toUpperCase();
-        this.modo = modo; 
-        this.alteracion = alteracion;
-    }
-}
-
-let ordenSostenidos = new Escalas("FA#, DO#, SOL#, RE#, LA#, MI#, SI#", 'cromatico', 'sostenidos');
-let ordenBemoles = new Escalas("SIb, MIb, LAb, REb, SOLb, DOb, FAb", 'cromatico', 'bemoles');
-
-//escalas mayores con sostenido
-//reemplazada con array asociativo
-
-let escalaDo = new Escalas("DO, RE, MI, FA, SOL, LA, SI", 'mayor', 'sostenidos');
-let escalaRe = new Escalas("RE, MI, FA#, SOL, LA, SI, DO#", 'mayor', 'sostenidos');
-let escalaSol = new Escalas("SOL, LA, SI, DO, RE, MI, FA#", 'mayor', 'sostenidos');
-let escalaLa = new Escalas("LA, SI, DO#, RE, MI, FA#, SOL#", 'mayor', 'sostenidos');
-let escalaMi = new Escalas("MI, FA#, SOL#, LA, SI, DO#, RE#", 'mayor', 'sostenidos');
-let escalaSi = new Escalas("SI, DO#, RE#, MI#, FA#, SOL#, LA#", 'mayor', 'sostenidos');
-let escalaFaSostenido = new Escalas("FA#, SOL#, LA#, SI, DO#, RE#, MI#", 'mayor', 'sostenidos');
-let escalaDoSostenido = new Escalas("DO#, RE#, MI#, FA#, SOL#, LA#, SI#", 'mayor', 'sostenidos');
-
-
-
-//escalas mayores con bemoles
-let escalaFa = new Escalas("FA, SOL, LA, SIb, DO, RE, MI", 'mayor', 'bemoles');
-let escalaSib = new Escalas("SIb, DO, RE, MIb, FA, SOL, LA", 'mayor', 'bemoles');
-let escalaMib = new Escalas("MIb, FA, SOL, LAb, SIb, DO, RE", 'mayor', 'bemoles');
-let escalaLab = new Escalas("LAb, SIb, DO, REb, MIb, FA, SOL", 'mayor', 'bemoles');
-let escalaReb = new Escalas("REb, MIb, FA, SOLb, LAb, SIb, DO", 'mayor', 'bemoles');
-let escalaSolb = new Escalas("SOLb, LAb, SIb, DOb, REb, MIb, FA", 'mayor', 'bemoles');
-let escalaDob = new Escalas("DOb, REb, MIb, FAb, SOLb, LAb, SIb", 'mayor', 'bemoles');
-
-//escalas menores naturales con sostenidos
-let escalaLaMenor = new Escalas("LA, SI, DO, RE, MI, FA, SOL", "menor", "sostenidos");
-let escalaMiMenor = new Escalas("MI, FA#, SOL, LA, SI, DO, RE", "menor", "sostenidos");
-let escalaSiMenor = new Escalas("SI, DO#, RE, MI, FA#, SOL, LA", "menor", "sostenidos");
-let escalaFaSosMenor = new Escalas("FA#, SOL#, LA, SI, DO#, RE, MI", "menor", "sostenidos");
-let escalaDoSosMenor = new Escalas("DO#, RE#, MI, FA#, SOL#, LA, SI", "menor", "sostenidos");
-let escalaSolSosMenor = new Escalas("SOL#, LA#, SI, DO#, RE#, MI, FA#", "menor", "sostenidos");
-let escalaReSosMenor = new Escalas("RE#, MI#, FA#, SOL#, LA#, SI, DO#", "menor", "sostenidos");
-let escalaLaSosMenor = new Escalas("LA#, SI#, DO#, RE#, MI#, FA#, SOL#", "menor", "sostenidos");
-
-//escalas menores naturales con bemoles
-
-let escalaReMenor = new Escalas("RE, MI, FA, SOL, LA, SIb, DO", "menor", "bemoles");
-let escalaSolMenor = new Escalas("SOL, LA, SIb, DO, RE, MIb, FA", "menor", "bemoles");
-let escalaDoMenor = new Escalas("DO, RE, MIb, FA, SOL, LAb, SIb", "menor", "bemoles");
-let escalaFaMenor = new Escalas("FA, SOLb, LAb, SIb, DO, REb, MIb", "menor", "bemoles");
-let escalaSibMenor = new Escalas("SIb, DO, REb, MIb, FA, SOLb, LAb", "menor", "bemoles");
-let escalaMibMenor = new Escalas("MIb, FA, SOLb, LAb, SIb, DOb, REb", "menor", "bemoles");
-let escalaLabMenor = new Escalas("LAb, SIb, DOb, REb, MIb, FAb, SOLb", "menor", "bemoles");
-
-
-//Agrupo las variables creadas con array asociativo, y de esta manera ahorro codigo funcional y puedo utilizar las diferentes escalas por separado mucho mejor.
-//Proximamente pasare cada escala a un array diferente.
-
-let escalasMayConSos = [escalaDo, escalaSol, escalaRe, escalaLa, escalaMi, escalaSi, escalaFaSostenido, escalaDoSostenido];
-let escalasMayConBem = [escalaFa, escalaSib, escalaMib, escalaLab, escalaReb, escalaSolb, escalaDob];
-let escalasMenNatConSos = [escalaLaMenor, escalaMiMenor, escalaSiMenor, escalaFaSosMenor, escalaDoSosMenor, escalaSolSosMenor, escalaReSosMenor, escalaLaSosMenor];
-let escalasMenNatConBem = [escalaReMenor, escalaSolMenor, escalaFaMenor,escalaSibMenor, escalaMibMenor, escalaLabMenor];
-
-
-//....
-
-let sostenidos = [escalaDo, escalaSol, escalaRe, escalaLa, escalaMi, escalaSi, escalaFaSostenido, escalaDoSostenido, escalaLaMenor, escalaMiMenor, escalaSiMenor, escalaFaSosMenor, escalaDoSosMenor, escalaSolSosMenor, escalaReSosMenor, escalaLaSosMenor];
-
-
-let bemoles = [escalaFa, escalaSib, escalaMib, escalaLab, escalaReb, escalaSolb, escalaDob, escalaReMenor, escalaSolMenor, escalaDoMenor, escalaFaMenor, escalaSibMenor, escalaMibMenor, escalaLabMenor]
-
-let cromatico = [ordenBemoles, ordenSostenidos]
-//....
-
-
-const todasLasEscalas = [ordenSostenidos, ordenBemoles, escalaDo, escalaSol, escalaRe, escalaLa, escalaMi, escalaSi, escalaFaSostenido, escalaDoSostenido, escalaFa, escalaSib, escalaMib. escalaLab, escalaReb, escalaSolb, escalaDob, escalaLaMenor, escalaMiMenor, escalaSiMenor, escalaFaSosMenor, escalaDoSosMenor, escalaSolSosMenor, escalaReSosMenor, escalaLaSosMenor, escalaReMenor, escalaSolMenor, escalaDoMenor, escalaFaMenor, escalaSibMenor, escalaMibMenor, escalaLabMenor]; 
-//...................
-
-console.log(todasLasEscalas);
 
 const selectOptions = document.getElementById('verEscalas');
 const mostrar = document.querySelector('#mostrar');
 const viewResult = document.getElementById('resultado');
 
 
-const filtro = (array, alteracion) => {return array.filter(item => item.alteracion == alteracion) }
-
-
-const handleClick = (array, select) => {
-    return filtro(array, select.value);
-}
-
 const viewHTML = (array, lista) => {
     lista.innerHTML = '';
     array.forEach(escalas => {
-        let li = `<li>${escalas.notas}, modo ${escalas.modo} $ ${escalas.alteracion}</li>`;
+        let li = `<li> Las notas son: ${escalas.notas}. <br> El modo de esta escala es: ${escalas.modo} <br> Las alterciones son: ${escalas.alteracion}</li>`
         lista.innerHTML += li;
-    });
+    })
 } 
+console.log(viewHTML);
 
-mostrar.onclick = () => {
-    viewHTML(handleClick(todasLasEscalas, selectOptions), viewResult);
-}
+mostrar.addEventListener('click', ()=>{
+    const escalas={
+        ordenBemoles: [ordenBemoles],
+        ordenBemoles: [ordenSostenidos],
+        escalasMayConSos,
+        escalasMayConBem,
+        escalasMenNatConSos,
+        escalasMenNatConBem
+    }
+    viewHTML(escalas[selectOptions.value], viewResult);
+});
+    
