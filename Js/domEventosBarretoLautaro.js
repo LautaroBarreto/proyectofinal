633e0d780fcc3aaf174ef304ab8a9084f4dc25f2
Lautaro Barreto <102369702+LautaroBarreto@users.noreply.github.com>
2022-08-23 22:36:22 -0300

    Add files via upload

--- Js/domEventosBarretoLautaro.js
+++ Js/domEventosBarretoLautaro.js
@@ -3,6 +3,7 @@ const selectOptions = document.getElementById('verEscalas');
 const mostrar = document.querySelector('#mostrar');
 const viewResult = document.getElementById('resultado');
 
+const getLocalStorge = (key) => JSON.parse(localStorage.getItem(key))
 
 const viewHTML = (array, lista) => {
     lista.innerHTML = '';
@@ -10,17 +11,16 @@ const viewHTML = (array, lista) => {
         let li = `<li> Las notas son: ${escalas.notas}. <br> El modo de esta escala es: ${escalas.modo} <br> Las alterciones son: ${escalas.alteracion}</li>`
         lista.innerHTML += li;
     })
-} 
-console.log(viewHTML);
+}
 
 mostrar.addEventListener('click', ()=>{
     const escalas={
-        ordenBemoles: [ordenBemoles],
-        ordenBemoles: [ordenSostenidos],
-        escalasMayConSos,
-        escalasMayConBem,
-        escalasMenNatConSos,
-        escalasMenNatConBem
+        ordenBemoles: JSON.parse(localStorage.getItem('ordenBemoles')),
+        ordenSostenidos: JSON.parse(localStorage.getItem('ordenSostenidos')),
+        escalasMayConSos: JSON.parse(localStorage.getItem('escalasMayConSos')),
+        escalasMayConBem: JSON.parse(localStorage.getItem('escalasMayConBem')),
+        escalasMenNatConSos: JSON.parse(localStorage.getItem('escalasMenNatConSos')),
+        escalasMenNatConBem: JSON.parse(localStorage.getItem('escalasMenNatConBem'))
     }
     viewHTML(escalas[selectOptions.value], viewResult);
 });
